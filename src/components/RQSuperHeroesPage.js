8989991bf142281473b3aac9fdafbd8755f55618
Thinh Nguyen <t9ngdu00@students.oamk.fi>
2024-08-21 15:58:56 +0700

    Data transform

--- src/components/RQSuperHeroesPage.js
+++ src/components/RQSuperHeroesPage.js
@@ -10,20 +10,24 @@ const RQSuperHeroesPage = () => {
   //   return axios.get("http://localhost:4000/superheroes");
   // });
 
-  const onSuccess = () => {
-    console.log('Perform side effect after data fetching')
-  }
+  const onSuccess = (data) => {
+    console.log("Perform side effect after data fetching", data);
+  };
 
-  const onError = () => {
-    console.log('Perform side effect after data encourtering')
-  }
+  const onError = (error) => {
+    console.log("Perform side effect after data encourtering", error);
+  };
 
   const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     "super-heroes",
     fetchSuperHeroes,
     {
       onSuccess: onSuccess,
-      onError: onError
+      onError: onError,
+      select: (data) => {
+        const superHeroNames = data.data.map((hero) => hero.name);
+        return superHeroNames;
+      },
     }
   );
 
@@ -41,8 +45,12 @@ const RQSuperHeroesPage = () => {
     <>
       <h2>RQ Super Heroes Page</h2>
       <button onClick={refetch}>Fetch heroes</button>
-      {data?.data.map((hero) => {
+      {/* {data?.data.map((hero) => {
         return <div key={hero.name}>{hero.name}</div>;
+      })} */}
+
+      {data.map((heroName) => {
+        return <div key={heroName}>{heroName}</div>;
       })}
     </>
   );
